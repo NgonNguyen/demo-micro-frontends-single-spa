5591dd6cd0b64f67caebeb2419ed8174c141357f
NgonNguyen <ntngon7@gmail.com>
2023-03-31 14:52:14 +0700

    remove folders

--- src/app-root-config.ts
+++ src/app-root-config.ts
@@ -5,8 +5,19 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
+import loading from "../loading.gif";
 
-const routes = constructRoutes(microfrontendLayout);
+const routes = constructRoutes(microfrontendLayout, {
+  errors: {
+    mfError: "Oops! The micro-frontend isn't working right now"
+  },
+  loaders: {
+    // mainContentLoader: `<img src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.16.1/images/loader-large.gif" />`
+    mainContentLoader: `<img src="${loading}" />`
+  },
+  props: {}
+});
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
